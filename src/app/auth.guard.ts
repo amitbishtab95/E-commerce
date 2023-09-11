c68dc77633f2ef7267aab96fcd29318e2b9c222c
Your Name <amitbishtab95@gmail.com>
2023-09-11 07:42:43 +0530

    updates

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,8 @@
+import { CanActivateFn } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  constructor(){
+
+  }
+  return false;
+};
---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { SignUp } from '../data-types';
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.scss']
+})
+export class SellerAuthComponent {
+
+  constructor(private seller: SellerService, private router: Router) {
+
+  }
+  signUp(data: SignUp): void {
+    console.log(data);
+    this.seller.userSignUp(data)
+    // .subscribe((result) => {
+    //   console.log("result", result);
+    //   if (result) {
+    //     this.router.navigate(['seller-home'])
+    //   }
+    // })
+  }
+}
