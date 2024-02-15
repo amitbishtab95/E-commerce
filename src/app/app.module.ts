1f2a6c62b759860d37bc286d37d3b3ecd58eeb30
Amit Bisht <amitbishtab95@gmail.com>
2024-02-15 12:21:49 +0530

    Api integration on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SellerAddProductComponent } from './seller-add-product/seller-add-produ
 import { SellerProductListComponent } from './seller-product-list/seller-product-list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { SellerUpdateProductComponent } from './seller-update-product/seller-upd
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-types';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  
+  popularProducts : undefined | product[];
+  trendyProducts : undefined | product[]
+  constructor(public product:ProductService){
 
+  }
+
+  ngOnInit():void{
+    this.product.popularProducts().subscribe((data)=>{
+      this.popularProducts = data;
+    })
+    this.product.trendyProducts().subscribe((data)=>{
+      this.trendyProducts=data;
+    })
+  }
 }
